477d56d85b5e834f92fe2f2cc2b48c4b92c81bbd
sl5net <sl5softwarelab@gmail.com>
2022-11-23 12:35:32 +0100

    restart creating this app

---
+++ app/src/main/java/de/sl5/counterUnitGuide0ad/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.sl5.counterUnitGuide0ad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
