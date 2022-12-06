59b2a1903105ebf59d6c965a37d85f18ad336026
sl5net <sl5softwarelab@gmail.com>
2022-12-06 05:08:31 +0100

    add Toast to some Buttons

--- app/src/main/java/de/sl5/counterUnitGuide0ad/MainActivity.kt
+++ app/src/main/java/de/sl5/counterUnitGuide0ad/MainActivity.kt
@@ -1,29 +1,26 @@
 package de.sl5.counterUnitGuide0ad
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<ImageButton>(R.id.r1col1).setOnClickListener {
-            toastContentDescription(it)
+//        findViewById<ImageButton>(R.id.r1col1).setOnClickListener {
+//            toastContentDescription(it)
+        for (i in 1..7) {
+            val idR1: Int = resources.getIdentifier("r1col$i", "id", this.packageName)
+            val idR2: Int = resources.getIdentifier("r2col$i", "id", this.packageName)
+            findViewById<ImageButton>(idR1).setOnClickListener {
+                toastContentDescription(it)
+            }
+            findViewById<ImageButton>(idR2).setOnClickListener {
+                toastContentDescription(it)
+            }
         }
-        findViewById<ImageButton>(R.id.r1col2).setOnClickListener {
-            toastContentDescription(it)
-        }
-        findViewById<ImageButton>(R.id.r1col3).setOnClickListener {
-            toastContentDescription(it)
-        }
-
-
     }
-
     private fun toastContentDescription(it: View) {
         val contentDescription = it.contentDescription
         val myToast = Toast.makeText(applicationContext, contentDescription, Toast.LENGTH_SHORT)
